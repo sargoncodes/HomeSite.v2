3e265784cd9084870d8c59c93cb580ef1e3fb26c
Timmy <jamesbcarlile@gmail.com>
2025-09-02 22:47:56 -0500

    Made it more dynamic

--- __init__.py
+++ __init__.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template, send_from_directory
+from sys import argv
 #import os
 
+if (len(argv) < 3 or len(argv) > 4):
+    print("\n[X] Error: Incorrect number of arguments\nUsage: __init__.py <Listener IP> <Listening Port> [debug] \n")
+    quit()
+elif (len(argv) == 4) and (str(argv[3]).upper() == "DEBUG"):
+    db=True
+else:
+    db=False
+
+
 app = Flask(__name__)
-ip = "192.168.4.99"
-port = 80
+ip = str(argv[1])
+port = int(argv[2])
 
 @app.route("/", methods=["GET"])
 def landing():
@@ -17,4 +27,4 @@ def books():
 def send_test():
     return send_from_directory(app.config['static/media/books'], 'test.pdf')
 
-if __name__ == "__main__": app.run(debug=True, host=ip, port=port)
+if __name__ == "__main__": app.run(debug=db, host=ip, port=port)
